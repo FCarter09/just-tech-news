54f30f3715ad060cff43bd68cfdd74db2a771751
Forest Carter <fc_vstate@yahoo.com>
2022-08-18 11:59:42 -0400

    update branch

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
@@ -47,6 +48,19 @@ User.init(
       }
     },
     {
+     // hashes password
+     hooks: { 
+       // set up beforeCreate lifecycle "hook" functionality
+        async beforeCreate(newUserData) {
+          newUserData.password = await bcrypt.hash(newUserData.password, 10);
+          return newUserData;
+        },
+         // set up beforeUpdate lifecycle "hook" functionality
+        async beforeUpdate(updatedUserData) {
+          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+          return updatedUserData;
+        }
+      },
       // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
         // pass in our imported sequelize connection (the direct connection to our database)
@@ -60,6 +74,7 @@ User.init(
         // make it so our model stays lowercase in the database
         modelName: 'user'
     }
+    
 );
 
 module.exports = User;
