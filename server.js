62278bee0c5a96cc60cf9797d7296a6d8afb7963
FCarter09 <fc_vstate@yahoo.com>
2022-08-22 15:50:22 -0400

    update homepage-view

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const exphbs = require('express-handlebars');
 const sequelize = require('./config/connection');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
 
+//set up handlebars for html template
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//middleware 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(routes);
